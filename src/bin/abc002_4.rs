114c243f9ec3d882d550984646bdac4ac5fddca2
Akira Sosa <akirasosa@gmail.com>
2020-05-04 10:47:03 +0700

    try using proconio

--- src/bin/abc002_4.rs
+++ src/bin/abc002_4.rs
@@ -10,54 +10,8 @@ use std::f64::consts::PI;
 use std::io::Write;
 use std::ops::{AddAssign, Sub};
 
-macro_rules! input {
-    (source = $s:expr, $($r:tt)*) => {
-        let mut iter = $s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-    ($($r:tt)*) => {
-        let s = {
-            use std::io::Read;
-            let mut s = String::new();
-            std::io::stdin().read_to_string(&mut s).unwrap();
-            s
-        };
-        let mut iter = s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-}
-
-macro_rules! input_inner {
-    ($iter:expr) => {};
-    ($iter:expr, ) => {};
-
-    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
-        let $var = read_value!($iter, $t);
-        input_inner!{$iter $($r)*}
-    };
-}
-
-macro_rules! read_value {
-    ($iter:expr, ( $($t:tt),* )) => {
-        ( $(read_value!($iter, $t)),* )
-    };
-
-    ($iter:expr, [ $t:tt ; $len:expr ]) => {
-        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-    };
-
-    ($iter:expr, chars) => {
-        read_value!($iter, String).chars().collect::<Vec<char>>()
-    };
-
-    ($iter:expr, usize1) => {
-        read_value!($iter, usize) - 1
-    };
-
-    ($iter:expr, $t:ty) => {
-        $iter.next().unwrap().parse::<$t>().expect("Parse error")
-    };
-}
+use proconio::input;
+use proconio::fastout;
 
 macro_rules! debug {
     ($($e:expr),*) => {
@@ -387,6 +341,7 @@ fn div_euclid(a: i64, rhs: i64) -> i64 {
     q
 }
 
+#[proconio::fastout]
 fn main() {
     input! {
         N: usize,
